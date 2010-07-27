9c344196140298c7154b73f0b38212b3ecca57f1
Andrew Rosen <Retsej@gatech.edu>
2010-07-27 18:39:48 +0000

    framework for TrigClassifier is constructed

--- gov/nasa/ial/mde/solver/classifier/TrigClassifier.java
+++ gov/nasa/ial/mde/solver/classifier/TrigClassifier.java
@@ -1,5 +1,50 @@
 package gov.nasa.ial.mde.solver.classifier;
 
-public class TrigClassifier extends MDEClassifier {
+import java.util.ArrayList;
 
+import gov.nasa.ial.mde.solver.SolvedGraph;
+import gov.nasa.ial.mde.solver.symbolic.AnalyzedEquation;
+
+public class TrigClassifier extends MDEClassifier {
+	
+	private boolean hasSin = false,  
+					hasCos = false, 
+					hasTan = false,
+					hasMultiples =false,
+					hasX = false;
+	
+	public ArrayList<String> arr;
+	
+	public TrigClassifier() {
+		super();
+		
+		
+	}
+	
+	
+	
+	public SolvedGraph getFeatures(AnalyzedEquation analyzedEquation) {
+		SolvedGraph features = null;
+		
+		detectTrig(analyzedEquation);
+		
+		if(hasSin){
+			features =null;
+		}
+		
+		return features;
+	}
+
+
+
+	private void detectTrig(AnalyzedEquation analyzedEquation) {
+		String equat = analyzedEquation.getInputEquation();
+		
+		hasSin = equat.contains("sin");
+		hasCos = equat.contains("cos");
+		hasTan = equat.contains("tan");
+		
+		
+	}
+	
 }
