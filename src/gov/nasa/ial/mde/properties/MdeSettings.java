93ee569f125a4bc6333b0fcedf8b85e039fedf77
Benjamin Davison <davison@gatech.edu>
2010-07-28 17:19:25 +0000

    MDE: updated "resources" folder to be in the source folder.  This will make the resources accessible within the MDE.  There may be problems with packaging things into a JAR.  Updated MdeSettings with the new resources path.

--- src/gov/nasa/ial/mde/properties/MdeSettings.java
+++ src/gov/nasa/ial/mde/properties/MdeSettings.java
@@ -131,7 +131,7 @@ public class MdeSettings extends Settings {
                                 (BETA_RELEASE ? " (Beta-"+BETA_VERSION+")" : "");
 
     /** The path to the MDE resources such as images, XSL, and HTML files. */
-    public static final String  RESOURCES_PATH = "/resources/";
+    public static final String  RESOURCES_PATH = "/";
     
     /** Array of parameters and default values represented as strings */
     public static final String[][] PARAMETER_STRINGS = {
