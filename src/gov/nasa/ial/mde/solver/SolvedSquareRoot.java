94775d81a4a4a8c70ff3fc61f9662856bd38b7a4
Andrew Rosen <Retsej@gatech.edu>
2010-08-02 17:17:30 +0000

    Added todo for sqrt

--- src/gov/nasa/ial/mde/solver/SolvedSquareRoot.java
+++ src/gov/nasa/ial/mde/solver/SolvedSquareRoot.java
@@ -10,8 +10,7 @@ import gov.nasa.ial.mde.solver.symbolic.Polynomial;
 public class SolvedSquareRoot extends SolvedXYGraph {
 
 	protected String[] newFeatures = { 
-            "vertex", "slope"
-            };
+            "vertex" };
 	
 	protected PolynomialClassifier PC;
 
@@ -21,13 +20,7 @@ public class SolvedSquareRoot extends SolvedXYGraph {
 		
 		
 		
-		//can be solved by making some assumptions
-		
-		
-		
-		
-		
-		
+		//can be solved by making some assumptions	
 		PC = (PolynomialClassifier) ae.getClassifier();
 		Polynomial poly = ae.getLhs();
 		
@@ -42,78 +35,23 @@ public class SolvedSquareRoot extends SolvedXYGraph {
 		
 		parts[0]= parts[0] +")";
 		
-		//System.out.print("ae.toString Equation: " + equat);
-		
-		
-		//equat
 		
 		
 		
+		//Get the inner part of the SQRT( _______)
 		
-		/*
-		 * 
-		QuadraticClassifier QC=new QuadraticClassifier(poly);
-		System.out.println("\n " + QC.getNormalizedEquation());
-		double[] coeffs =QC.getNormalizedCoefficients();
-		for(int i = 0 ; i<coeffs.length ; i++)
-		{
-			System.out.println(coeffs[i]);
-		}
-		System.out.println("Inside a SolvedSquareRoot");
-		System.out.println("Polynomial is :" + poly);
+		//Send that part thru the MDE
+		
+		// get vertex
+		/* assuming the form is sqrt(mx + b) + c, 
+		 * x intercept is the -b/m
+		 * y intercept is  c 
+		 */
 		
 		
+		double yVertice = Double.valueOf(parts[1]);
 		
 		
-		//here we need to calculate the vertex
-	
 				
-		
-		
-		
-		*
-		 * 
-		 * case RIGHT :
-                vertexUV[1] = transUV[1];
-                vertexUV[0] = coeffs[4];
-                axisInclination = alpha;
-                break;
-
-            case LEFT :
-                vertexUV[1] = transUV[1];
-                vertexUV[0] = coeffs[4];
-                axisInclination = alpha + 180.0;
-                break;
-		 * 
-		 * 
-		
-		
-		
-		
-		IntervalXY D = new IntervalXY(analyzedEq.getActualVariables()[0], Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-        D.setEndPointExclusions(IntervalXY.EXCLUDE_LOW_X | IntervalXY.EXCLUDE_HIGH_X);
-        putFeature("domain", D);
-		
-		*/
-        
-        
-	//	Expression[] coeffs =poly.getCoefficientsAsExpressions("x");
-	
-		//System.out.println(coeffs[0]);
-		
-		//System.out.println("TEST POINT");
-		//initialize();
-		
-		
-		
-		
-		
-	}
-
-	private void initialize() {
-		
-		
-		 //PointXY vertex = new PointXY(QC.UV2XY(vertexUV));
-		 //putFeature("vertex", vertex);
 	}
 }
