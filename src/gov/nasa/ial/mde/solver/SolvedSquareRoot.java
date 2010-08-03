f79ca15e697719bacffbab06aca1f68098f9b7d1
Andrew Rosen <Retsej@gatech.edu>
2010-08-03 16:39:46 +0000

    attempted to make some changes with the XML.  That did not work as planned.  I'm uploading what I have now to make a record of it.  I have, however, figured out how to do it without XML.

--- src/gov/nasa/ial/mde/solver/SolvedSquareRoot.java
+++ src/gov/nasa/ial/mde/solver/SolvedSquareRoot.java
@@ -1,7 +1,19 @@
 package gov.nasa.ial.mde.solver;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import gov.nasa.ial.mde.describer.Describer;
 import gov.nasa.ial.mde.math.IntervalXY;
@@ -26,24 +38,18 @@ public class SolvedSquareRoot extends SolvedXYGraph {
 		//can be solved by making some assumptions	
 		PC = (PolynomialClassifier) ae.getClassifier();
 		Polynomial poly = ae.getLhs();
-		
 		String equat= ae.printOriginalEquation();
-		
 		String[] parts =equat.split("\\)");
 		
 		for(int i = 0; i < parts.length;i++)
 		{
 			System.out.println(parts[i]);
 		}
-		
 		parts[0]= parts[0] +")";
 		
 		
 		//Get the inner part of the SQRT( _______)
-		
-		
 		String insideSQRT = "sqrt\\(([^)\\n]*)\\)";
-		
 		String innerEquat=parts[0].replaceAll(insideSQRT,"$1");
 		
 	
@@ -53,13 +59,52 @@ public class SolvedSquareRoot extends SolvedXYGraph {
 		solver.add(innerEquat);
 	    solver.solve();
 	    
+	    
+	    
+	    
 	    //TODO: make an isLinear check for a lot of this
 	    Solution solution = solver.get(0);
-	    String innerFeatures =solution.getFeatures().getXMLString();
+	    String xmlString =solution.getFeatures().getXMLString(); 
+	    String[] innerFeatures = xmlString.split("\\*x|x");
 	    
 	    
-	    //get Slope and "yIntercept"
-	   // double slope= Double.valueOf(innerFeatures.);
+	    System.out.println(xmlString);
+	    /*
+	     * 
+	     * DocumentBuilderFactory factory =
+	                DocumentBuilderFactory.newInstance();
+	           
+	                DocumentBuilder builder =null;
+					try {
+						builder = factory.newDocumentBuilder();
+					} catch (ParserConfigurationException e) {
+						e.printStackTrace();
+					}
+	                try {
+	                	System.out.println("In try");
+						Document document = builder.parse(new InputSource(new StringReader(xmlString)));
+						NodeList list = document.getElementsByTagName("I don't exist");
+						
+						
+						for(int i=0;i<list.getLength();i++)
+						{
+							System.out.println(list.item(i).toString());
+						}
+					} catch (SAXException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+	     * 
+	     * 
+	     * 
+	     * 
+	     * 
+	     * 
+	    */
+					
+		//get Slope and "yIntercept"
+		//double slope= Double.valueOf(innerFeatures.);
 	    
 	   
 		
@@ -68,7 +113,7 @@ public class SolvedSquareRoot extends SolvedXYGraph {
 		 * x intercept is the -b/m
 		 * y intercept is  c 
 		 */
-		
+		double slope;
 	    double xVertice;
 		double yVertice;
 		
