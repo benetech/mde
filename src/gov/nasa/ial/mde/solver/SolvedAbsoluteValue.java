cc8ea31e65ac828a22d14ee1843401b95c12cdde
Andrew Rosen <Retsej@gatech.edu>
2010-08-06 19:40:25 +0000

    SolvedAbsoluteValue now supports Domain, Range, the direction an abs of a linear equation opens, as well as a vertex

--- src/gov/nasa/ial/mde/solver/SolvedAbsoluteValue.java
+++ src/gov/nasa/ial/mde/solver/SolvedAbsoluteValue.java
@@ -1,8 +1,10 @@
 package gov.nasa.ial.mde.solver;
 
+import gov.nasa.ial.mde.math.IntervalXY;
 import gov.nasa.ial.mde.math.PointXY;
 import gov.nasa.ial.mde.solver.classifier.PolynomialClassifier;
 import gov.nasa.ial.mde.solver.classifier.QuadraticClassifier;
+import gov.nasa.ial.mde.solver.features.individual.SlopeFeature;
 import gov.nasa.ial.mde.solver.symbolic.AnalyzedEquation;
 import gov.nasa.ial.mde.solver.symbolic.Equation;
 import gov.nasa.ial.mde.solver.symbolic.Expression;
@@ -12,7 +14,7 @@ import gov.nasa.ial.mde.solver.symbolic.RationalExpression;
 public class SolvedAbsoluteValue extends SolvedXYGraph{
 	
 	protected String[] newFeatures = { 
-            "vertex"};
+            "vertex", "direction"};
 	
 	protected PolynomialClassifier PC;
 
@@ -41,18 +43,71 @@ public class SolvedAbsoluteValue extends SolvedXYGraph{
 	    solver.solve();   
 	    
 	    Solution solution = solver.get(0);
-	    String xmlString =solution.getFeatures().getXMLString(); 
-	    //TODO: make an isLinear check for a lot of this.  Perhaps we need a LinearAbs and a LinearSQRT
-	    //TODO: actually putfeatures could be handled on different functions in this class 
+	    SolvedGraph features = solution.getFeatures();
+	
+	    if(features instanceof SolvedLine){		    
+		    //for a linear equation 
+		    //form of a*|mx+b| + c
+		    // vertex is ( -ab/m, c)
+		    // slope of am
+	    	
+	    	double slope =Double.NaN;
+		    double intercept=Double.NaN;
+		    double xVertice = Double.NaN;
+		    double yVertice = Double.NaN;
+		    
+		    slope=((SlopeFeature) features).getSlope();
+		    
+		    if(parts.length>=2)
+			{
+				yVertice = Double.valueOf(parts[1]);
+			}
+			else
+			{
+				yVertice= 0;
+			}
+		    
+		    intercept =((SolvedLine) features).getYIntercept();
+	    	
+	    	String getCoeff = "(-?\\d*\\.?\\d*)\\*abs";
+	    	double coeff= 1;
+	    	parts[0]=parts[0].replace("y", "");
+	    	parts[0]=parts[0].replace("=", "");
+	    	parts[0]=parts[0].replace(" ", "");
+	    	
+
+	    	String temp= parts[0].replaceFirst(getCoeff, "$1----");
+	    	if(temp.contains("----")){
+	    		coeff = Double.valueOf((temp.split("----")[0]));
+	    	}
+	    	xVertice = -(intercept*coeff)/slope;
+	    	
+	    	IntervalXY D, R;
+	    	PointXY vertex = new PointXY( new double[]{xVertice,yVertice});
+	    	D = new IntervalXY(analyzedEq.getActualVariables()[0], Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+	    	
+	    	String direction;
+	    	if(coeff>0){
+                R = new IntervalXY(analyzedEq.getActualVariables()[1], yVertice, Double.POSITIVE_INFINITY);
+                direction="up";
+	    	}else{
+	    		R = new IntervalXY(analyzedEq.getActualVariables()[1], Double.NEGATIVE_INFINITY, yVertice);
+                direction="down";
+	    	}
+	    	
+	    	
+	    	putNewFeatures(newFeatures);
+			putFeature("vertex", vertex);
+			putFeature("direction", direction);
+			putFeature("domain", D);
+			putFeature("range", R);
+	    	
+	    }
 	    
 	    
-	    //for a linear equation 
-	    //form of a|mx+b| + c
-	    // vertex is ( -ab/m, c)
-	    // slope of am
+	    
 	        
 		
-
 	}
 	
 
