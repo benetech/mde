3fb1d8da3f4c6238988fb49005e50ac791430cb9
Andrew Rosen <Retsej@gatech.edu>
2010-10-08 18:57:40 +0000

    Output is done, followed by: 3. Processing 4. Make presentable 5. Cleanup 6. ??? 7. PROFIT!

--- src/gov/nasa/ial/mde/ui/DescriberPanel.java
+++ src/gov/nasa/ial/mde/ui/DescriberPanel.java
@@ -5,7 +5,10 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class DescriberPanel extends JPanel {
 	/**
@@ -14,9 +17,13 @@ public class DescriberPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JLabel instructions;
 	private JTextField input;
+	private JTextArea output;
+	
 	
 	public DescriberPanel(){
+		//Instructions
 		instructions = new JLabel("Enter equation and press enter:");
+		//input
 		input = new JTextField(20);
 		input.addKeyListener(new KeyAdapter(){
 			public void keyPressed(KeyEvent e){
@@ -24,13 +31,27 @@ public class DescriberPanel extends JPanel {
 				if(val == KeyEvent.VK_ENTER){
 					System.out.println("\n\n"+input.getText());
 					input.setText("");
-					System.out.println("ENTER pressed");
+					output.setText(processEquation("2"));
 				}
 			}
 		});
+		//setup output
+		output = new JTextArea("Your description will appear here.");
+		output.setEditable(false);
+		
+		
+		
+		
+		
+		//add components
 		this.add(instructions);
 		this.add(input);
-		//this.add(output);
+		this.add(output);
 		
+	}
+	
+	
+	private String processEquation(String equation){
+		return equation;
 	}
 }
