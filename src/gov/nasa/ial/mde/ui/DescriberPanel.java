29e6fee154377ddd28cc6f700359eea7bb4f8eed
Andrew Rosen <Retsej@gatech.edu>
2010-10-08 19:19:26 +0000

    Processing is done, followed by: 4. Make presentable 5. Cleanup 6. ??? 7. PROFIT!

--- src/gov/nasa/ial/mde/ui/DescriberPanel.java
+++ src/gov/nasa/ial/mde/ui/DescriberPanel.java
@@ -1,6 +1,10 @@
 package gov.nasa.ial.mde.ui;
 
 
+import gov.nasa.ial.mde.describer.Describer;
+import gov.nasa.ial.mde.properties.MdeSettings;
+import gov.nasa.ial.mde.solver.Solver;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JLabel;
@@ -18,23 +22,29 @@ public class DescriberPanel extends JPanel {
 	private JLabel instructions;
 	private JTextField input;
 	private JTextArea output;
-	
+	private MdeSettings currentSettings;
+	private Solver solver;
+	private Describer describer;
 	
 	public DescriberPanel(){
+		
+		setupMDE();
+		
 		//Instructions
 		instructions = new JLabel("Enter equation and press enter:");
+		
 		//input
 		input = new JTextField(20);
 		input.addKeyListener(new KeyAdapter(){
 			public void keyPressed(KeyEvent e){
 				int val = e.getKeyCode();
 				if(val == KeyEvent.VK_ENTER){
-					System.out.println("\n\n"+input.getText());
+					output.setText(processEquation(input.getText()));
 					input.setText("");
-					output.setText(processEquation("2"));
 				}
 			}
 		});
+		
 		//setup output
 		output = new JTextArea("Your description will appear here.");
 		output.setEditable(false);
@@ -51,7 +61,33 @@ public class DescriberPanel extends JPanel {
 	}
 	
 	
-	private String processEquation(String equation){
-		return equation;
+	private void setupMDE() {
+		this.currentSettings = new MdeSettings("myAppsMdeProperties");
+		this.solver = new Solver();
+		this.describer = new Describer(solver, currentSettings);
+		
+		//we can change this if we wanted to change what
+		this.describer.setOutputFormat(Describer.TEXT_OUTPUT);   
 	}
+
+
+	private String processEquation(String equation){
+		String description ="OH DEAR GOD\n FULL TILT\nPLEASE RESTART\n";
+		try{
+			solver.add(equation);
+			solver.solve();
+			
+			if (solver.anyDescribable()) {
+				description = describer.getDescriptions("standards");
+			} else {
+				description="MDE could not generate a description for " + equation + ".";
+			}
+			solver.removeAll();
+			
+		}catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+		return description;
+    }
 }
