cb2bfdc497dbd12edcd5029b90a9c57e9328ee9b
Andrew Rosen <Retsej@gatech.edu>
2010-10-26 19:09:11 +0000

    Added new class to make the Accessible Conversion conversion modular.

---
+++ src/gov/nasa/ial/mde/util/AccessibleTTSConverter.java
@@ -0,0 +1,13 @@
+package gov.nasa.ial.mde.util;
+
+public class AccessibleTTSConverter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
